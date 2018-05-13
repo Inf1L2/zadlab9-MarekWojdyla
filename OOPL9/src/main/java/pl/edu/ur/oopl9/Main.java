cd1af9d7a85468fc9d91dc2798e64cb69b82b369
Anonim <Anonim@DESKTOP-LOLI5NV>
2018-05-13 23:06:46 +0200

    Zad3

--- OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
@@ -1,21 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pl.edu.ur.oopl9;
 
-/**
- *
- * @author maarchyl
- */
-public class Main {
+import java.util.Random;
 
-    /**
-     * @param args the command line arguments
-     */
+
+
+public class Main {
+public static void b_sort(int tab[]){
+int temp;
+int zmiana = 1;
+while(zmiana > 0){
+zmiana = 0;
+for(int i=0; i<tab.length-1; i++){
+if(tab[i]>tab[i+1]){
+temp = tab[i+1];
+tab[i+1] = tab[i];
+tab[i] = temp;
+zmiana++;
+}
+}
+}
+for(int i=0; i<tab.length; i++){
+System.out.print(tab[i]+" ");
+}
+}
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+          Random r = new Random(); // import java.util.Random; 
+int[][] array = new int[10][10]; 
+int c = 100; 
+
+for (int a = 0; a < array.length; a++) 
+{ 
+for (int b = 0; b < array[a].length; b++) 
+{ 
+array[a][b] = r.nextInt(c); 
+} 
+
+c += 100; 
+} 
+
+for (int a = 0; a < array.length; a++) 
+{ 
+
+for (int b = 0; b < array[a].length; b++) 
+{ 
+System.out.print(array[a][b] + "\t"); 
+} 
+System.out.print("Po sortowaniu :");
+b_sort(array[a]);
+
+System.out.println(); 
+
+
+
+
+} 
+
+}
     }
     
-}
+
